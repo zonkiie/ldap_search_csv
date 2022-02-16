713c6dba6a065ec0506c3316e5286bf3efd6104f
Rainer Datzer <rdatz@gmx.de>
2022-02-16 03:39:19 +0100

    working on dynamic loading

--- dynamic_loading.c
+++ dynamic_loading.c
@@ -3,8 +3,19 @@
 void *dlhandle;
 char *ldap_lib_path;
 
+void cleanup_ldap_dynfunctions()
+{
+	dlclose(dlhandle);
+}
+
 void load_dynfunctions()
 {
 	dlhandle = dlopen(ldap_lib_path, RTLD_LAZY);
 	atexit(cleanup_ldap_dynfunctions);
+}
+
+void init_ldap_dynfunctions()
+{
+	ldap_simple_bind_s = (int(*)(LDAP *, const char *, const char *)) dlsym(dlhandle, "ldap_simple_bind_s");
+	ldap_sasl_bind_s = (int(*)(LDAP *, const char *, const char *,  struct berval *, LDAPControl **, LDAPControl **, struct berval **)) dlsym(dlhandle, "ldap_sasl_bind_s");
 }
