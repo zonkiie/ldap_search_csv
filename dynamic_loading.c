afb0685c8df0861b135cacb135244173cb7638b6
Rainer Datzer <rdatz@gmx.de>
2022-02-18 20:58:20 +0100

    improved dynamic loading

--- dynamic_loading.c
+++ dynamic_loading.c
@@ -1,39 +1,43 @@
 #include <dynamic_loading.h>
 
-void *dlhandle;
-char *ldap_lib_path;
+void *ldap_dlhandle = NULL;
+void *ber_dlhandle = NULL;
+char *ldap_lib_path = NULL;
+char *ber_lib_path; = NULL;
 
 void cleanup_ldap_dynfunctions()
 {
-	dlclose(dlhandle);
+	if(ldap_dlhandle) dlclose(ldap_dlhandle);
+	if(ber_dlhandle) dlclose(ber_dlhandle);
 }
 
 void load_dynfunctions()
 {
-	dlhandle = dlopen(ldap_lib_path, RTLD_LAZY);
+	ldap_dlhandle = dlopen(ldap_lib_path, RTLD_LAZY);
+	ber_dlhandle = dlopen(ber_lib_path, RTLD_LAZY);
 	atexit(cleanup_ldap_dynfunctions);
 }
 
 void init_ldap_dynfunctions()
 {
-	ldap_simple_bind_s = (int(*)(LDAP *, const char *, const char *)) dlsym(dlhandle, "ldap_simple_bind_s");
-	ldap_sasl_bind_s = (int(*)(LDAP *, const char *, const char *,  struct berval *, LDAPControl **, LDAPControl **, struct berval **)) dlsym(dlhandle, "ldap_sasl_bind_s");
-	ldap_unbind_ext_s = (int(*)(LDAP *ld, LDAPControl *sctrls[], LDAPControl *cctrls[])) dlsym(dlhandle, "ldap_unbind_ext_s");
-	ldap_msgfree = (int(*)( LDAPMessage *msg )) dlsym(dlhandle, "ldap_msgfree");
-	ldap_result = (int(*)( LDAP *ld, int msgid, int all, struct timeval *timeout, LDAPMessage **result )) dlsym(dlhandle, "ldap_result");
-	ldap_search_ext = (int(*)( LDAP *ld, char *base, int scope, char *filter, char *attrs[], int attrsonly, LDAPControl **serverctrls, LDAPControl **clientctrls, struct timeval *timeout, int sizelimit, int *msgidp )) dlsym(dlhandle, "ldap_search_ext");
-	ldap_first_attribute = (char*(*)(LDAP *ld, LDAPMessage *entry, BerElement **berptr)) dlsym(dlhandle, "ldap_first_attribute");
-	ldap_next_attribute = (char*(*)(LDAP *ld, LDAPMessage *entry, BerElement *ber)) dlsym(dlhandle, "ldap_next_attribute");
-	ldap_memfree = (void(*)(void* p)) dlsym(dlhandle, "ldap_memfree");
-	ldap_initialize = (int(*)(LDAP **ldp, char *uri)) dlsym(dlhandle, "ldap_initialize");
-	ldap_set_option = int(*)(LDAP *ld, int option, const void *invalue)) dlsym(dlhandle, "ldap_set_option");
-	ldap_err2string = (char*(*)(int err)) dlsym(dlhandle, "ldap_err2string");
-	ldap_get_dn = (char*(*)(LDAP *ld, LDAPMessage *entry)) dlsym(dlhandle, "ldap_get_dn");
-	ldap_get_values_len = (struct berval **(*)(LDAP *ld, LDAPMessage *entry, char *attr)) dlsym(dlhandle, "ldap_get_values_len");
-	ldap_parse_reference = (int(*)(LDAP *ld, LDAPMessage *reference, char ***referralsp, LDAPControl ***serverctrlsp, int freeit )) dlsym(dlhandle, "ldap_parse_reference");
-	ldap_parse_result = (int(*)(LDAP *ld, LDAPMessage *result, int *errcodep, char **matcheddnp, char **errmsgp, char ***referralsp, LDAPControl ***serverctrlsp, int freeit)) dlsym(dlhandle, "ldap_parse_result");
-	ber_free = (void (*)(BerElement *ber, int freebuf)) dlsym(dlhandle, "ber_free");
-	ber_bvfree = (void (*)(struct berval *bv)) dlsym(dlhandle, "ber_bvfree");
-	ber_bvstrdup = (struct berval *(*)(const char *str)) dlsym(dlhandle, "ber_bvstrdup");
-	ber_bvecfree = (void (*)(struct berval **bvec)) dlsym(dlhandle, "ber_bvecfree");
+	ldap_simple_bind_s = (int(*)(LDAP *, const char *, const char *)) dlsym(ldap_dlhandle, "ldap_simple_bind_s");
+	ldap_sasl_bind_s = (int(*)(LDAP *, const char *, const char *,  struct berval *, LDAPControl **, LDAPControl **, struct berval **)) dlsym(ldap_dlhandle, "ldap_sasl_bind_s");
+	ldap_unbind_ext_s = (int(*)(LDAP *ld, LDAPControl *sctrls[], LDAPControl *cctrls[])) dlsym(ldap_dlhandle, "ldap_unbind_ext_s");
+	ldap_msgfree = (int(*)( LDAPMessage *msg )) dlsym(ldap_dlhandle, "ldap_msgfree");
+	ldap_result = (int(*)( LDAP *ld, int msgid, int all, struct timeval *timeout, LDAPMessage **result )) dlsym(ldap_dlhandle, "ldap_result");
+	ldap_search_ext = (int(*)( LDAP *ld, char *base, int scope, char *filter, char *attrs[], int attrsonly, LDAPControl **serverctrls, LDAPControl **clientctrls, struct timeval *timeout, int sizelimit, int *msgidp )) dlsym(ldap_dlhandle, "ldap_search_ext");
+	ldap_first_attribute = (char*(*)(LDAP *ld, LDAPMessage *entry, BerElement **berptr)) dlsym(ldap_dlhandle, "ldap_first_attribute");
+	ldap_next_attribute = (char*(*)(LDAP *ld, LDAPMessage *entry, BerElement *ber)) dlsym(ldap_dlhandle, "ldap_next_attribute");
+	ldap_memfree = (void(*)(void* p)) dlsym(ldap_dlhandle, "ldap_memfree");
+	ldap_initialize = (int(*)(LDAP **ldp, char *uri)) dlsym(ldap_dlhandle, "ldap_initialize");
+	ldap_set_option = int(*)(LDAP *ld, int option, const void *invalue)) dlsym(ldap_dlhandle, "ldap_set_option");
+	ldap_err2string = (char*(*)(int err)) dlsym(ldap_dlhandle, "ldap_err2string");
+	ldap_get_dn = (char*(*)(LDAP *ld, LDAPMessage *entry)) dlsym(ldap_dlhandle, "ldap_get_dn");
+	ldap_get_values_len = (struct berval **(*)(LDAP *ld, LDAPMessage *entry, char *attr)) dlsym(ldap_dlhandle, "ldap_get_values_len");
+	ldap_parse_reference = (int(*)(LDAP *ld, LDAPMessage *reference, char ***referralsp, LDAPControl ***serverctrlsp, int freeit )) dlsym(ldap_dlhandle, "ldap_parse_reference");
+	ldap_parse_result = (int(*)(LDAP *ld, LDAPMessage *result, int *errcodep, char **matcheddnp, char **errmsgp, char ***referralsp, LDAPControl ***serverctrlsp, int freeit)) dlsym(ldap_dlhandle, "ldap_parse_result");
+	ber_free = (void (*)(BerElement *ber, int freebuf)) dlsym(ber_dlhandle, "ber_free");
+	ber_bvfree = (void (*)(struct berval *bv)) dlsym(ber_dlhandle, "ber_bvfree");
+	ber_bvstrdup = (struct berval *(*)(const char *str)) dlsym(ber_dlhandle, "ber_bvstrdup");
+	ber_bvecfree = (void (*)(struct berval **bvec)) dlsym(ber_dlhandle, "ber_bvecfree");
 }
